8cbf95fc11e76fb047c92b7a72ff0e70d3018485
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-06-10 19:08:56 +0000

    ad manager 2.0 RC
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/ad-manager/trunk@1727 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- ad-manager.php
+++ ad-manager.php
@@ -3,7 +3,7 @@
 Plugin Name: Ad Manager
 Plugin URI: http://www.semiologic.com/software/ad-manager/
 Description: A widget-based ad unit manager. Combine with inline widgets and widget contexts to get the most of it.
-Version: 1.1 alpha
+Version: 2.0 RC
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 Text Domain: ad-manager-info
@@ -20,156 +20,135 @@ http://www.mesoconcepts.com/license/
 **/
 
 
-class ad_manager
-{
-	#
-	# init()
-	#
-	
-	function init()
-	{
-		add_action('widgets_init', array('ad_manager', 'widgetize'), 0);
-		
-		if ( !is_admin() )
-		{
-			add_action('init', array('ad_manager', 'set_cookie'));
-		}
-	} # init()
-	
-	
-	#
-	# widgetize()
-	#
-	
-	function widgetize()
-	{
-		$options = ad_manager::get_options();
-		
-		$widget_options = array('classname' => 'ad_unit', 'description' => __( "A generic ad unit") );
-		$control_options = array('width' => 500, 'id_base' => 'ad_unit');
-		
-		$id = false;
+load_plugin_textdomain('ad-manager', null, dirname(__FILE__) . '/lang');
 
-		# registered widgets
-		foreach ( array_keys($options) as $o )
-		{
-			if ( !is_numeric($o) ) continue;
-			$id = "ad_unit-$o";
 
-			wp_register_sidebar_widget($id, __('Ad Unit'), array('ad_manager', 'display_widget'), $widget_options, array( 'number' => $o ));
-			wp_register_widget_control($id, __('Ad Unit'), array('ad_manager_admin', 'widget_control'), $control_options, array( 'number' => $o ) );
-		}
+/**
+ * ad_manager
+ *
+ * @package Ad Manager
+ **/
+
+add_action('widgets_init', array('ad_manager', 'widgets_init'));
+
+if ( !is_admin() ) {
+	add_action('init', array('ad_manager', 'set_cookie'));
+}
+
+class ad_manager extends WP_Widget {
+	/**
+	 * set_cookie()
+	 *
+	 * @return void
+	 **/
+
+	function set_cookie() {
+		if ( is_feed() || current_user_can('unfiltered_html') || defined('WP_CACHE') )
+			return;
 		
-		# default widget if none were registered
-		if ( !$id )
-		{
-			$id = "ad_unit-1";
-			wp_register_sidebar_widget($id, __('Ad Unit'), array('ad_manager', 'display_widget'), $widget_options, array( 'number' => -1 ));
-			wp_register_widget_control($id, __('Ad Unit'), array('ad_manager_admin', 'widget_control'), $control_options, array( 'number' => -1 ) );
-		}
-	} # widgetize()
-	
-	
-	#
-	# set_cookie()
-	#
-	
-	function set_cookie()
-	{
-		if ( !is_feed() && !current_user_can('unfiltered_html') && !defined('WP_CACHE') )
-		{
-			if ( !$_COOKIE['am_visit_counted_' . COOKIEHASH] )
-			{
-				# count visit (and expire in a month)
-				setcookie(
-					'am_visits_' . COOKIEHASH,
-					$_COOKIE['am_visits_' . COOKIEHASH]++,
-					time() + 14 * 86400,
-					COOKIEPATH,
-					COOKIE_DOMAIN
-					);
-				
-				$_COOKIE['am_visit_counted_' . COOKIEHASH] = 1;
-				
-				setcookie(
-					'am_visits_' . COOKIEHASH,
-					1,
-					null,
-					COOKIEPATH,
-					COOKIE_DOMAIN
-					);
-			}
+		if ( !isset($_COOKIE['am_visit_counted_' . COOKIEHASH]) ) {
+			return;
+			
+			# count visit (and expire in two weeks)
+			setcookie(
+				'am_visits_' . COOKIEHASH,
+				$_COOKIE['am_visits_' . COOKIEHASH]++,
+				time() + 14 * 86400,
+				COOKIEPATH,
+				COOKIE_DOMAIN
+				);
+			
+			setcookie(
+				'am_visit_counted_' . COOKIEHASH,
+				1,
+				null,
+				COOKIEPATH,
+				COOKIE_DOMAIN
+				);
 		}
 	} # set_cookie()
 	
 	
-	#
-	# display_widget()
-	#
+	/**
+	 * widgets_init()
+	 *
+	 * @return void
+	 **/
+
+	function widgets_init() {
+		register_widget('ad_manager');
+	} # widgets_init()
 	
-	function display_widget($args, $widget_args = 1)
-	{
-		extract( $args, EXTR_SKIP );
-		if ( is_numeric($widget_args) )
-			$widget_args = array( 'number' => $widget_args );
-		$widget_args = wp_parse_args( $widget_args, array( 'number' => -1 ) );
-		extract( $widget_args, EXTR_SKIP );
+	
+	/**
+	 * ad_manager()
+	 *
+	 * @return void
+	 **/
 
-		$options = ad_manager::get_options();
+	function ad_manager() {
+		$widget_ops = array(
+			'classname' => 'ad_unit',
+			'description' => __('An ad unit. Combine with Inline Widgets for use in posts.', 'ad-manager'),
+			);
+		$control_ops = array(
+			'width' => 430,
+			);
 		
-		if ( !isset($options[$number]) )
-			return;
+		$this->WP_Widget('ad_unit', __('Ad Widget', 'ad-manager'), $widget_ops, $control_ops);
+	} # ad_manager()
+	
+	
+	/**
+	 * widget()
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 * @return void
+	 **/
 
-		extract($options[$number]);
+	function widget($args, $instance) {
+		extract($args, EXTR_SKIP);
+		$instance = wp_parse_args($instance, ad_manager::defaults());
+		extract($instance, EXTR_SKIP);
 		
-		# admin: just return a title
-		
-		if ( is_admin() )
+		if ( is_admin() || !$code || is_404() || is_feed() )
 			return;
-		
-		# bypass all of this if there's no code at all or if we're on a 404 or in a feed
-		
-		if ( !$code || is_404() || is_feed() ) return;
 		
 		# check preconditions from the least to most expensive in CPU cycles
 		
 		# precondition: irregular visitor
 		
-		if ( $casual_visitor )
-		{
-			if ( !defined('WP_CACHE') && $_COOKIE['am_visits_' . COOKIEHASH] > 3 ) return;
+		if ( $casual_visitor ) {
+			if ( !defined('WP_CACHE') && $_COOKIE['am_visits_' . COOKIEHASH] > 3 )
+				return;
 		}
 		
 		# precondition: old post
 		
-		if ( $old_post )
-		{
-			if ( is_single() )
-			{
-				$post = get_post($GLOBALS['wp_the_query']->get_queried_object_id());
-			}
-			elseif ( in_the_loop() && ( is_home() && !is_front_page() || !is_page() ) )
-			{
-				$post = get_post(get_the_ID());
+		if ( $old_post && !is_page() ) {
+			if ( in_the_loop() ) {
+				global $post;
+			} elseif ( is_single() ) {
+				global $wp_the_query;
+				$post = $wp_the_query->get_queried_object();
+			} else {
+				$post = false;
 			}
 			
-			if ( $post )
-			{
+			if ( $post ) {
 				$post_date = mysql2date('U', $post->post_date_gmt);
-				
 				$two_weeks_ago = gmdate('U') - 14 * 86400;
-
-				if ( $post_date > $two_weeks_ago ) return;
+				if ( $post_date > $two_weeks_ago )
+					return;
 			}
 		}
 
 		# precondition: search engine
 		
-		if ( $search_engine )
-		{
-			if ( !defined('WP_CACHE')
-				&& strpos($_SERVER['HTTP_REFERER'], $_SERVER['REQUEST_URI']) === false
-				&& preg_match("/^
+		if ( $search_engine && !defined('WP_CACHE') && isset($_SERVER['HTTP_REFERER'])
+			&& strpos($_SERVER['HTTP_REFERER'], $_SERVER['HTTP_HOST']) === false ) {
+			if ( preg_match("/^
 					https?:\/\/
 					[^\/]*
 					(?:					# an easy to spot search engine
@@ -190,28 +169,26 @@ class ad_manager
 						\b
 					|
 						.+
-						\b				# a frequently used parameter
-						(s|q)
+						(?:[?&])		# a frequently used parameter
+						(?:s|q)
 						=
 					)
 				/ix", $_SERVER['HTTP_REFERER'])
 				)
-			{
 				return;
-			}
 		}
 		
 		# precondition: php
 		
-		if ( $php_condition )
-		{
+		if ( $php_condition ) {
 		#	dump($php_code);
 			eval("\$bool = ( $php_code );");
 			
-			if ( !$bool ) return;
+			if ( !$bool )
+				return;
 		}
 		
-		# make sure it's a valid ad before displaying anything
+		# precondition: AUP
 		
 		# https://www.google.com/adsense/support/bin/answer.py?answer=48182#pla
 		static $adsense_units = 0;
@@ -223,135 +200,198 @@ class ad_manager
 		# https://publisher.yahoo.com/legal/prog_policy.php
 		static $ypn_units = 0;
 		
-		if ( strpos($code, '<!-- SiteSearch Google -->') !== false )
-		{
-			if ( ++$google_site_searches > 2 ) return;
-		}
-		elseif ( strpos($code, 'google_ad_client') !== false )
-		{
+		if ( strpos($code, '<!-- SiteSearch Google -->') !== false ) {
+			if ( ++$google_site_searches > 2 )
+				return;
+		} elseif ( strpos($code, 'google_ad_client') !== false ) {
 			# adsense referral: google_ad_format = "ref_text";
 			# adsense link: google_ad_format = "120x90_0ads_al_s";
 			
-			if ( !preg_match("/
+			if ( preg_match("/
 				google_ad_format
 				\s*
 				=
 				\s*
-				([\"'])(.*)\\1
-				/Uix", $code, $match)
-				)
-			{
-			}
+				([\"'])(.*?)\\1
+				/ix", $code, $match)
+				) {
+				$type = strtolower(end($match));
 
-			$type = strtolower(end($match));
-			
-			if ( $type == 'ref_text' )
-			{
-				if ( ++$adsense_referrals > 3 ) return;
+				if ( $type == 'ref_text' ) {
+					if ( ++$adsense_referrals > 3 )
+						return;
+				} elseif ( strpos($type, 'ads_al') !== false ) {
+					if ( ++$adsense_links > 3 )
+						return;
+				} else {
+					if ( ++$adsense_units > 3 )
+						return;
+				}
 			}
-			elseif ( strpos($type, 'ads_al') !== false )
-			{
-				if ( ++$adsense_links > 3 ) return;
-			}
-			else
-			{
-				if ( ++$adsense_units > 3 ) return;
-			}
-		}
-		elseif ( strpos($code, 'ctxt_ad_partner') !== false )
-		{
-			if ( ++$ypn_units > 3 ) return;
+		} elseif ( strpos($code, 'ctxt_ad_partner') !== false ) {
+			if ( ++$ypn_units > 3 )
+				return;
 		}
 		
 		# editors see place holders so they don't click on their own ads, as do authors on drafts
 		
-		if ( current_user_can('unfiltered_html') || is_preview() )
-		{
-			$code = '<div style="'
-				. 'color: Black; background: GhostWhite; border: dotted 1px SteelBlue; padding: 20px;'
-				. '">' . "\n"
-				. sprintf('<strong>%s</strong> place holder. To see the "real" ad, either log off, or open this url in a different browser.', $title)
+		if ( current_user_can('unfiltered_html') || is_preview() ) {
+			$code = '<div style="color: Black; background: GhostWhite; border: dotted 1px SteelBlue; padding: 20px;">' . "\n"
+				. sprintf(__('You\'re viewing a place holder for an ad unit (<code>%s</code>) because you are logged in. Please log out to see the real ad.', 'ad-manager'), $title)
 				. '</div>' . "\n";
 		}
 		
-		# apply style preferences
-		
-		if ( $float )
-		{
-			$code = '<div style="'
-				. ( $float == 'left'
-					? 'float: left; margin: 0px .5em .1em 0px;'
-					: 'float: right; margin: 0px 0px .1em .5em;'
-					)
-					. '">' . "\n"
-				. $code
-				. '</div>' . "\n";
-		}
-		
-		# display
-		
-		echo $before_widget . "\n"
+		echo $before_widget
 			. $code
-			. $after_widget . "\n";
-	} # display_widget()
+			. $after_widget;
+	} # widget()
 	
 	
-	#
-	# get_options()
-	#
-	
-	function get_options()
-	{
-		if ( !( $o = get_option('ad_manager') ) )
-		{
-			$o = array();
-			update_option('ad_manager', $o);
-		}
+	/**
+	 * update()
+	 *
+	 * @param array $new_instance
+	 * @param array $old_instance
+	 * @return array $instance
+	 **/
+
+	function update($new_instance, $old_instance) {
+		$instance = array();
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['code'] = current_user_can('unfiltered_html')
+			? $new_instance['code']
+			: $old_instance['code'];
+		foreach ( array('search_engine', 'old_post', 'casual_visitor', 'php_condition') as $var )
+			$instance[$var] = isset($new_instance[$var]);
+		$instance['php_code'] = current_user_can('unfiltered_html')
+			? $new_instance['php_code']
+			: $old_instance['php_code'];
 		
-		return $o;
-	} # get_options()
-	
-	
-	#
-	# new_widget()
-	#
-	
-	function new_widget($args = null)
-	{
-		$o = ad_manager::get_options();
-		$k = time();
-		$o[$k] = ad_manager::default_options();
-		$o[$k] = array_merge($o[$k], (array) $args);
+		if ( $instance['php_condition'] && !trim($instance['php_code']) )
+			$instance['php_condition'] = false;
 		
-		update_option('ad_manager', $o);
+		return $instance;
+	} # update()
+	
+	
+	/**
+	 * form()
+	 *
+	 * @param array $instance
+	 * @return void
+	 **/
+
+	function form($instance) {
+		$instance = wp_parse_args($instance, ad_manager::defaults());
+		extract($instance, EXTR_SKIP);
+	
+		echo '<p>'
+			. '<label>'
+			. __('Title (never displayed to visitors)', 'ad-manager') . '<br />'
+			. '<input type="text" class="widefat"'
+				. ' id="' . $this->get_field_id('title') . '"'
+				. ' name="' . $this->get_field_name('title') . '"'
+				. ' value="' . esc_attr($title) . '"'
+				. ' />'
+			. '</label>'
+			. '</p>' . "\n";
 		
-		return 'ad_unit-' . $k;
-	} # new_widget()
+		echo '<textarea class="widefat code" rows="8" cols="20"'
+				. ' name="' . $this->get_field_name('code') . '"'
+				. ( !current_user_can('unfiltered_html')
+					? ' disabled="disabled"'
+					: ''
+					)
+				. '>'
+			. esc_html($code)
+			. '</textarea>' . "\n";
+		
+		echo '<h3>' . __('Context', 'ad-manager') . '</h3>' . "\n";
+		
+		echo '<p>'
+			. __('Only display this ad unit when any of these conditions are met:', 'ad-manager')
+			. '</p>' . "\n";
+		
+		echo '<p>'
+			. '<label>'
+			. '<input type="checkbox" name="' . $this->get_field_name('search_engine') . '"'
+				. ( $search_engine
+					? ' checked="checked"'
+					: ''
+					)
+				. ' />'
+			. '&nbsp;'
+			. __('The visitor comes from a search engine', 'ad-manager')
+			. '</label>'
+			. '</p>' . "\n";
+		
+		echo '<p>'
+			. '<label>'
+			. '<input type="checkbox" name="' . $this->get_field_name('old_post') . '"'
+				. ( $old_post
+					? ' checked="checked"'
+					: ''
+					)
+				. ' />'
+			. '&nbsp;'
+			. __('The post (not page) is more that 2 weeks old', 'ad-manager')
+			. '</label>'
+			. '</p>' . "\n";
+		
+		echo '<p>'
+			. '<label>'
+			. '<input type="checkbox" name="' . $this->get_field_name('casual_visitor') . '"'
+				. ( $casual_visitor
+					? ' checked="checked"'
+					: ''
+					)
+				. ' />'
+			. '&nbsp;'
+			. __('The visitor is not a regular reader (three visits in the past two weeks)', 'ad-manager')
+			. '</label>'
+			. '</p>' . "\n";
+		
+		echo '<p>'
+			. '<label>'
+			. '<input type="checkbox" name="' . $this->get_field_name('php_condition') . '"'
+				. ' id="' . $this->get_field_id('php_condition') . '"'
+				. ( $php_condition
+					? ' checked="checked"'
+					: ''
+					)
+				. ' />'
+			. '&nbsp;'
+			. __('The following php condition (<a href="http://codex.wordpress.org/Conditional_Tags" target="_blank">WordPress conditional tags</a>) is met:', 'ad-manager')
+			. '</label>' . '</p>' . "\n";
+		
+		echo '<textarea class="widefat code" rows="3" cols="20" name="ad_unit[' . $number .'][php_code]"'
+				. ' onchange="if ( this.value ) document.getElementById(\'' . $this->get_field_id('php_condition') . '\').checked = true; else document.getElementById(\'' . $this->get_field_id('php_condition') . '\').checked = false;"'
+				. ( !current_user_can('unfiltered_html')
+					? ' disabled="disabled"'
+					: ''
+					)
+				. '>'
+			. esc_html($php_code)
+			. '</textarea>' . "\n";
+	} # form()
 	
 	
-	#
-	# default_options()
-	#
-	
-	function default_options()
-	{
+	/**
+	 * defaults()
+	 *
+	 * @return array $instance
+	 **/
+
+	function defaults() {
 		return array(
-			'title' => 'Ad Unit',
-			'code' => 'Enter your ad unit code here',
+			'title' => __('Ad Unit', 'ad-manager'),
+			'code' => __('Enter your ad unit code here', 'ad-manager'),
 			'search_engine' => false,
 			'old_post' => false,
 			'casual_visitor' => false,
 			'php_condition' => false,
 			'php_code' => '',
-			'float' => false,
 			);
-	} # default_options()
+	} # defaults()
 } # ad_manager
-
-ad_manager::init();
-
-if ( is_admin() )
-{
-	include dirname(__FILE__) . '/ad-manager-admin.php';
-}
 ?>
