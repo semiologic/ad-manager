0858b22f7d2832910139c01656668bc4861d148a
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-07-10 16:12:24 +0000

    misc: fix incorrect load_plugin_textdomain calls
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/ad-manager/trunk@2047 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- ad-manager.php
+++ ad-manager.php
@@ -20,7 +20,7 @@ http://www.mesoconcepts.com/license/
 **/
 
 
-load_plugin_textdomain('ad-manager', null, dirname(__FILE__) . '/lang');
+load_plugin_textdomain('ad-manager', false, dirname(plugin_basename(__FILE__)) . '/lang');
 
 
 /**
